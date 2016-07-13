76891908f5c7d5acbca623365b5e4e9a9080886f
Jamie Bliss <astronouth7303@gmail.com>
2016-07-13 14:31:44 -0400

    Add Couchbase to InstanceManager.

--- app/src/main/java/com/astro73/timetrack/InstanceManager.java
+++ app/src/main/java/com/astro73/timetrack/InstanceManager.java
@@ -2,10 +2,15 @@ package com.astro73.timetrack;
 
 import android.content.Context;
 
+import com.couchbase.lite.CouchbaseLiteException;
+import com.couchbase.lite.Database;
+import com.couchbase.lite.Document;
 import com.couchbase.lite.Manager;
 import com.couchbase.lite.android.AndroidContext;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Central place to manage singletons. May eventually become IOC/whatever.
@@ -53,4 +58,31 @@ public class InstanceManager {
         return manager;
     }
 
+    private static String DATABASE = "timesheet";
+
+    public Database getDatabase() {
+        // This is only for development to populate demo data
+        if (!getDbManager().getAllDatabaseNames().contains(DATABASE)) {
+            try {
+                Database db = getDbManager().getDatabase(DATABASE);
+                for (TrackEntry te : TrackEntry.getDummyArray()) {
+                    Map<String, Object> properties = new HashMap<String, Object>();
+                    properties.put("_id", te.start.getTime());
+                    properties.put("description", te.description);
+                    properties.put("start", te.start.getTime());
+                    properties.put("end", te.start.getTime());
+                    Document doc = db.createDocument();
+                    doc.putProperties(properties);
+                }
+            } catch (CouchbaseLiteException e) {
+                throw new RuntimeException("Couldn't create default data", e);
+            }
+        }
+        try {
+            return getDbManager().getDatabase(DATABASE);
+        } catch (CouchbaseLiteException e) {
+            // FIXME: How do we handle this?
+            throw new RuntimeException("Can't open database", e);
+        }
+    }
 }
